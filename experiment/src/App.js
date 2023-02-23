9ff23c3e087b4a25c8f82e7bc3d1d021e0dc2970
Renzi <mengrenzi1996@gmail.com>
2023-02-22 16:40:33 -0800

    0222

---
+++ experiment/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Closure from './Closure.js';
+
+function App() {
+  return <Closure />;
+}
+
+export default App;
---
+++ experiment/src/Closure.js
@@ -0,0 +1,15 @@
+function Closure() {
+  init();
+  return <div></div>;
+}
+
+function init() {
+  var name = 'Mozilla'; // name is a local variable created by init
+  function displayName() {
+    // displayName() is the inner function, that forms the closure
+    console.log(name); // use variable declared in the parent function
+  }
+  displayName();
+}
+
+export default Closure;
